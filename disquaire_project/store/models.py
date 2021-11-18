0d0a53676c4a4284592b24f1d8db5fdd1f14d4e1
Drillin Pierre <sio.drillin@gmail.com>
2021-11-18 23:03:40 +0100

    Django suite initiation

--- disquaire_project/store/models.py
+++ disquaire_project/store/models.py
@@ -2,15 +2,24 @@ from django.db import models
 
 
 class Artist(models.Model):
+    def __str__(self):
+        return self.name
+
     name = models.CharField(max_length=200, unique=True)
 
 
 class Contact(models.Model):
+    def __str__(self):
+        return self.name
+
     email = models.EmailField(max_length=100)
     name = models.CharField(max_length=200)
 
 
 class Album(models.Model):
+    def __str__(self):
+        return self.title
+
     reference = models.IntegerField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     available = models.BooleanField(default=True)
@@ -19,6 +28,9 @@ class Album(models.Model):
     artists = models.ManyToManyField(Artist, related_name='albums', blank=True)
 
 class Booking(models.Model):
+    def __str__(self):
+        return self.contact.name
+
     created_at = models.DateTimeField(auto_now_add=True)
     contacted = models.BooleanField(default=False)
     album = models.OneToOneField(Album, on_delete=models.CASCADE)
