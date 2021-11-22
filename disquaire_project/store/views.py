d141bcb0eeb83ceea54ec815958545fc8cf11bc4
Drillin Pierre <sio.drillin@gmail.com>
2021-11-22 13:58:05 +0100

    Fin form + transaction

--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -1,7 +1,9 @@
+from django.db.utils import IntegrityError
 from django.shortcuts import render, get_object_or_404
 from .models import Album, Artist, Contact, Booking
-from .forms import ContactForm
+from .forms import ContactForm, ParagraphErrorList
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db import transaction
 
 
 # Create your views here.
@@ -32,6 +34,7 @@ def listing(request):
     }
     return render(request, 'store/listing.html', context)
 
+@transaction.atomic
 def detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
     artists = [artist.name for artist in album.artists.all()]
@@ -43,38 +46,44 @@ def detail(request, album_id):
         'thumbnail': album.picture
     }
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, error_class=ParagraphErrorList)
         if form.is_valid():
-            email = request.POST.get('email')
-            name = request.POST.get('name')
+            email = form.cleaned_data['email']
+            name = form.cleaned_data['name']
 
-            contact = Contact.objects.filter(email=email)
-            if not contact.exists():
-                # If a contact is not registered, create a new one.
-                contact = Contact.objects.create(
-                    email=email,
-                    name=name
-                )
+            try:
+                with transaction.atomic():
+                    contact = Contact.objects.filter(email=email)
+                    if not contact.exists():
+                        # If a contact is not registered, create a new one.
+                        contact = Contact.objects.create(
+                            email=email,
+                            name=name
+                        )
+                    else:
+                        contact = contact.first()
 
-            # If no album matches the id, it means the form must have been tweaked
-            # so returning a 404 is the best solution.
-            album = get_object_or_404(Album, id=album_id)
-            booking = Booking.objects.create(
-                contact=contact,
-                album=album
-            )
-
-            # Make sure no one can book the album again.
-            album.available = False
-            album.save()
-        else:
-            # Form data doesn't match the expected format.
-            # Add errors to the template.
-            context['errors'] = form.errors.items()
+                    album = get_object_or_404(Album, id=album_id)
+                    booking = Booking.objects.create(
+                        contact=contact,
+                        album=album
+                    )
+                    album.available = False
+                    album.save()
+                    context = {
+                        'album_title': album.title
+                    }
+                    return render(request, 'store/merci.html', context)
+            except IntegrityError:
+                form.errors['internal'] = "Une erreur interne est apparue. Merci de recommencer votre requête."
+        # else:
+        #     # Form data doesn't match the expected format.
+        #     # Add errors to the template.
+        #     context['errors'] = form.errors.items()
     else:
-        # GET method. Create a new form to be used in the template.
         form = ContactForm()
     context['form'] = form
+    context['errors'] = form.errors.items()
     return render(request, 'store/detail.html', context)
 
 
